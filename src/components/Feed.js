7a68f6fc104e49d4a7764e61b9439eb9e806c73d
Jasmin Sebastian <jasmins21995@gmail.com>
2024-01-23 22:51:31 +0530

    second commit

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,29 +1,34 @@
-// Feed.js
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setPosts } from '../redux/actions';
 import PostList from './PostList';
 import AddPostForm from './AddPostForm';
 import { Grid } from '@mui/material';
 import useStyles from './styles';
+import { fetchPosts } from '../services/api'; 
 
 const Feed = () => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   useEffect(() => {
     const getPosts = async () => {
-      if (posts.length === 0) {
+      try {
+        const apiPosts = await fetchPosts();
         const storedPosts = JSON.parse(localStorage.getItem('posts')) || [];
-        dispatch(setPosts(storedPosts));
-      } else {
-        localStorage.setItem('posts', JSON.stringify(posts));
+        if (storedPosts.length === 0) {
+          localStorage.setItem('posts', JSON.stringify(apiPosts));
+          dispatch(setPosts(apiPosts));
+        } else {
+          dispatch(setPosts(storedPosts));
+        }
+      } catch (error) {
+        console.error('Error loading or fetching posts:', error);
       }
     };
 
     getPosts();
-  }, [dispatch, posts]);
+  }, [dispatch]);
 
   return (
     <div className={classes.root}>
